c6a4ffc3733fc80ffe083db94a68ba103306ed8d
Jacob Williams <jayteedubya@gmail.com>
2024-02-03 17:03:43 -0800

    stubbed out linked list class

--- python/linear/linked_list.py
+++ python/linear/linked_list.py
@@ -2,3 +2,17 @@ class Node:
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
+
+
+class LinkedList[T]:
+    def __init__(self, *args: T) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def __getitem__(self, *args, **kwargs) -> T:
+        pass
+
+
+linky = LinkedList[str]()
